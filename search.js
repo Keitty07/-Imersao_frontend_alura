bf83e91287f6c8c23ec2a963cc6c132b35834cdd
Keitty Carla <116365639+Keitty07@users.noreply.github.com>
2025-02-04 16:21:35 -0300

    Adicionando arquivos do projeto

---
+++ search.js
@@ -0,0 +1,45 @@
+const resultArtist = document.getElementById("result-artist");
+const playlistContainer = document.getElementById("result-playlists");
+const searchInput = document.getElementById("search-input");
+
+function requestApi(searchTerm) {
+  fetch(`http://localhost:3000/artists?name_like=${searchTerm}`)
+    .then((response) => response.json())
+    .then((results) => displayResults(results));
+}
+
+function displayResults(results) {
+  hidePlaylists();
+  
+  // Se não houver resultados
+  if (results.length === 0) {
+    resultArtist.innerHTML = "<p>Nenhum artista encontrado.</p>";
+    resultArtist.classList.remove("hidden");
+    return;
+  }
+  
+  const artistImage = document.getElementById("artist-img");
+  const artistName = document.getElementById("artist-name");
+
+  results.forEach((element) => {
+    artistImage.src = element.urlImg;
+    artistName.innerText = element.name;
+  });
+
+  resultArtist.classList.remove("hidden");
+}
+
+
+function hidePlaylists() {
+  playlistContainer.classList.add("hidden");
+}
+
+searchInput.addEventListener("input", function () {
+  const searchTerm = searchInput.value.toLowerCase();
+  if (searchTerm === "") {
+    resultArtist.classList.add("hidden");
+    playlistContainer.classList.remove("hidden");
+    return;
+  }
+  requestApi(searchTerm);
+});
